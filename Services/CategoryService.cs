862ab80d181eb95ad110db6baa9383ebf8c5c093
pkerbynn <john.erbynn@gmail.com>
2020-03-12 17:34:45 +0000

    [wip]: add repository layer

---
+++ Services/CategoryService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+
+        }
+    }
+}
