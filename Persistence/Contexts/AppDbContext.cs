0842f38b5b9917d42a5cd9f25af881f5d3b90f74
pkerbynn <john.erbynn@gmail.com>
2020-03-12 19:51:53 +0000

    [feat]: add Db EntityFrameworkCore config

---
+++ Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        // property sets maping models to database tables
+        public DbSet<Category> Categories { get; set;}
+        public DbSet<Product> Products { get; set;}
+
+        // passing database configuration to base class through dependency injection
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // map the models’ properties to the respective table columns, primary keys, foreign keys, the column type
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Category>().ToTable("Categories");
+            builder.Entity<Category>().HasKey(p => p.Id);
+            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+            builder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
+
+            builder.Entity<Category>().HasData(
+                new Category { Id = 100, Name = "Fruits and Vegetables" },
+                                new Category { Id = 101, Name = "Dairy" }
+
+            );
+
+            builder.Entity<Product>().ToTable("Products");
+            builder.Entity<Product>().HasKey(p => p.Id);
+            builder.Entity<Product>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Entity<Product>().Property(p => p.QuantityInPackage).IsRequired();
+            builder.Entity<Product>().Property(p => p.UnitOfMeasurement).IsRequired();
+
+        }
+    }
+}
